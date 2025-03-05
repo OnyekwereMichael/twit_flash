3203f278d4bc8dea0a43d8f1fd72fadb20221a35
onyekweremichael <onyekweremichael55@gmail.com>
2025-03-05 07:41:08 -0800

    Refactor project configuration and add Tailwind CSS support with DaisyUI

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ["res.cloudinary.com"], // Allow images from Cloudinary
-  },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",  // Matches any request starting with /api/
-        destination: "http://localhost:5000/api/:path*", // Forwards to backend
-      },
-    ];
-  },
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
 export default nextConfig;
