68c9e5b72e2f1e92b8d40fce20a33ace3b1bd88e
onyekweremichael <onyekweremichael55@gmail.com>
2025-03-07 20:51:37 +0100

    Update API destination URL to use production backend

--- next.config.ts
+++ next.config.ts
@@ -7,7 +7,8 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",  // Matches any request starting with /api/
-        destination: "http://localhost:5000/api/:path*", // Forwards to backend
+        destination: "https://twit-flash-backend-1.onrender.com/api/:path*", // Forwards to backend
+        // http://localhost:5000
       },
     ];
   },
